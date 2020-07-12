e031f77a895bd0be1a5a98349628936ea2b75a06
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-07-11 23:10:32 -0700

    File cleanup
    Co-Authored-By: David Evans <53628042+evansdava@users.noreply.github.com>
    Co-Authored-By: pudgyeldergod <54594141+pudgyeldergod@users.noreply.github.com>

--- HowToSurviveThePolice/Controllers/BeforeCollectionViewController.swift
+++ HowToSurviveThePolice/Controllers/BeforeCollectionViewController.swift
@@ -14,25 +14,7 @@ class BeforeCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes DONT
-//        self.collectionView!.register(BeforeCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -80,20 +62,5 @@ class BeforeCollectionViewController: UICollectionViewController {
         newPageViewController.page = beforePages[indexPath.row]
         self.present(newPageViewController, animated: true, completion: nil)
     }
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
